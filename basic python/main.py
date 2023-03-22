756dfc57e3cfab5f38c8671c1dc6294228adb49a
sukhmani1303 <sukhmani1303@gmail.com>
2023-03-23 02:03:02 +0530

    mini_project 1 [BASIC CALCULATOR]

--- basic python/main.py
+++ basic python/main.py
@@ -1,10 +1,31 @@
 
+
+'''
 #  1st program -------
 
+print("Hello!")
+name = input("enter your name : ")
+print("Welcome",name)
+age = int(input("enter your age : "))
+a = input("what is your favourite dish?? ")
+print(a,"amazing!!!! Indeed it is delicious")
+print('a perfect taste for',age,'years old person')
 
-age = int(input("what is your age : "))
-if age >= 18:
-    print("you are an adult")
-    print("you can vote")
-else :
-    print("you cannot vote!!")
+'''
+
+
+a = int(input("enter a number : "))
+b = int(input("enter another number : "))
+ask = input("what operation do you want?? ")
+if ask=='sum':
+    print(a+b)
+elif a>b and ask=='sub':
+    print(a-b)
+elif b>a and ask=='sub':
+    print(b-a)
+elif (ask=='mul'):
+    print(a*b)
+elif (ask=='div'):
+    print(a/b)
+else: 
+    print(a%b)
