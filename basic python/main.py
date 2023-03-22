6aa4c4dd4688f053b430ea6a00bf54b62d2b3b39
sukhmani1303 <sukhmani1303@gmail.com>
2023-03-23 01:16:07 +0530

    1st program

---
+++ basic python/main.py
@@ -0,0 +1,10 @@
+
+#  1st program ---------
+
+print("Hello!")
+name = input("enter your name : ")
+print("Welcome",name)
+age = int(input("enter your age : "))
+a = input("what is your favourite dish?? ")
+print(a,"amazing!!!! Indeed it is delicious")
+print('a perfect taste for',age,'years old person')
