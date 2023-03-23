f6036be075ede65aaaae4bc42c314d0fbeed9c65
sukhmani1303 <sukhmani1303@gmail.com>
2023-03-24 00:58:43 +0530

    while loop practise

--- basic python/main.py
+++ basic python/main.py
@@ -11,8 +11,9 @@ a = input("what is your favourite dish?? ")
 print(a,"amazing!!!! Indeed it is delicious")
 print('a perfect taste for',age,'years old person')
 
-'''
 
+
+#MINI_PROJECT bASIC CALCULATOR
 
 a = int(input("enter a number : "))
 b = int(input("enter another number : "))
@@ -29,3 +30,84 @@ elif (ask=='div'):
     print(a/b)
 else: 
     print(a%b)
+'''
+
+
+#RANGE
+
+# The last number i.e. suppose 
+#  number = range(5)
+# here the numbers that will be printed would be 0,1,2,3,4 and '5' WILL NOT BE DISPLAYED
+
+
+# LOOPS
+ 
+# WHILE loop
+ # question 1
+
+'''i = 1
+while i<=10:
+    print(i)
+    print(i*i)
+    i = i+1
+'''
+# question 2
+
+'''i = 10
+while i<=300:
+    print(i)
+    i = i+10
+'''
+# question 3
+
+
+'''i = 105
+while i >=7:
+    print(i)
+    i = i - 7
+'''
+
+# question 4
+
+'''num = 2
+sum = 0
+while num <=20:
+    sum = sum + num
+    num = num+2
+    print(sum)
+'''
+
+# question 5
+
+'''
+num = int(input("enter a number : "))
+a = 0
+while a<10:
+    a = a + 1
+    print(num*a)
+
+'''
+
+# question 6
+
+a = int(input("enter a number : "))
+b = int(input("enter another number : "))
+
+# if a<b:
+#     while a<b:
+#         if a%2 ==0:
+#             print(a)
+#         a = a+1
+# else:
+#     while(a>b):
+#         if b%2==0:
+#             print(b)
+#         b = b+1
+
+start = min(a,b)
+end = max(a,b)
+
+while(start <= end):
+    if start%2==0:
+        print(start)
+    start+=1
